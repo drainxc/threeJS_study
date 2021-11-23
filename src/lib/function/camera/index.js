b94afe4cf40be3d62e9d42d9486e37fcfafaa31a
eastcopper <ldh7228@gmail.com>
2021-11-23 16:15:31 +0900

    📑 :: [Camera] Add SetupCamera

---
+++ src/lib/function/camera/index.js
@@ -0,0 +1,9 @@
+import * as THREE from "three";
+
+export function SetupCamera(element) {
+  const width = element.clientWidth;
+  const height = element.clientHeight;
+  const camera = new THREE.PerspectiveCamera(75, width / height, 1, 100); // camera 객체 생성
+  camera.position.z = 2;
+  return camera;
+}
