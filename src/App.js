f44ceaaeb438fda34738f7f201fe301fe3ed63ee
ldh7228 <ldh7228@gmail.com>
2021-11-23 16:22:14 +0900

    ⚡️ :: [App] Update light

--- src/App.js
+++ src/App.js
@@ -25,10 +25,7 @@ class Viewer extends React.Component {
     const camera = SetupCamera(this.element);
     this.camera = camera;
 
-    const color = "#ffffff"; // 광원의 색상
-    const intensity = 2; // 광원의 세기
-    const light = new THREE.DirectionalLight(color, intensity); // light 객체 생성
-    light.position.set(0, 2, 4);
+    const light = SetupLight();
     this.light = light;
 
     const model = SetupModel();
