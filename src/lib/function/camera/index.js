87c758ae834f7ee70ec47f80585b94fc73da130e
eastcopper <ldh7228@gmail.com>
2021-11-24 13:23:30 +0900

    ⚡️ :: [Camera] far

--- src/lib/function/camera/index.js
+++ src/lib/function/camera/index.js
@@ -3,7 +3,7 @@ import * as THREE from "three";
 export function SetupCamera(element) {
   const width = element.clientWidth;
   const height = element.clientHeight;
-  const camera = new THREE.PerspectiveCamera(75, width / height, 1, 100); // camera 객체 생성
-  camera.position.z = 20;
+  const camera = new THREE.PerspectiveCamera(75, width / height, 1, 4000); // camera 객체 생성
+  camera.position.set(600, 800, 500) 
   return camera;
 }
