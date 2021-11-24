6a9c55fa2d4d15baa8d76a7715cab6431bbce8b1
eastcopper <ldh7228@gmail.com>
2021-11-24 13:30:34 +0900

    ⚡️ :: [light] Add light2

--- src/lib/function/Light/index.js
+++ src/lib/function/Light/index.js
@@ -1,9 +1,19 @@
 import * as THREE from "three";
 
 export function SetupLight() {
-  const skyColor = 0xB1E1FF;  // light blue
-    const groundColor = 0xB97A20;  // brownish orange
-    const intensity = 1;
-    const light = new THREE.HemisphereLight(skyColor, groundColor, intensity);
+  const light1Color = 0xb1e1ff; // light blue
+  const light1GroundColor = 0xb97a20; // brownish orange
+  const intensity = 1;
+  const light1 = new THREE.HemisphereLight(light1Color, light1GroundColor, intensity);
+
+  const light2Color = 0xffffff;
+  const light2 = new THREE.DirectionalLight(light2Color, intensity);
+  light2.position.set(5, 10, 2);
+
+  const light = {
+    light1: light1,
+    light2: light2
+  }
+  
   return light;
 }
