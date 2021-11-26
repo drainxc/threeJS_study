ebcad324e5fc70e636a9e59d19e5b50e9c70f62b
eastcopper <ldh7228@gmail.com>
2021-11-26 12:52:46 +0900

    ⚡️ :: [GlftLoader] Add dumpObject

--- src/lib/function/glftLoader/index.js
+++ src/lib/function/glftLoader/index.js
@@ -7,5 +7,19 @@ export function GlftLoader(scene) {
     const root = gltf.scene;
     console.log(gltf);
     scene.add(root);
+    console.log(dumpObject(root).join('\n'));
   });
+}
+
+
+function dumpObject(obj, lines = [], isLast = true, prefix = '') {
+  const localPrefix = isLast ? '└─' : '├─';
+  lines.push(`${prefix}${prefix ? localPrefix : ''}${obj.name || '*no-name*'} [${obj.type}]`);
+  const newPrefix = prefix + (isLast ? '  ' : '│ ');
+  const lastNdx = obj.children.length - 1;
+  obj.children.forEach((child, ndx) => {
+    const isLast = ndx === lastNdx;
+    dumpObject(child, lines, isLast, newPrefix);
+  });
+  return lines;
 }
