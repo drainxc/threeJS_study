103a1665a1f994f421be0587a65031a5e0452851
eastcopper <ldh7228@gmail.com>
2021-11-26 12:45:20 +0900

    ⚡️ :: [GlftLoader] Add GLFTLoader

--- src/lib/function/glftLoader/index.js
+++ src/lib/function/glftLoader/index.js
@@ -1,4 +1,11 @@
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 export function GlftLoader(scene) {
-  
+  const gltfLoader = new GLTFLoader();
+  const url = "scene.gltf";
+  gltfLoader.load(url, (gltf) => {
+    const root = gltf.scene;
+    console.log(gltf);
+    scene.add(root);
+  });
 }
