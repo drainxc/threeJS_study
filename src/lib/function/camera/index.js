02814275c8cf9c38c97ceffe0db3a3e4fcfff405
eastcopper <ldh7228@gmail.com>
2021-11-28 22:52:46 +0900

    ⚡️ :: [Camera] Update sight and position

--- src/lib/function/camera/index.js
+++ src/lib/function/camera/index.js
@@ -3,7 +3,7 @@ import * as THREE from "three";
 export function SetupCamera(element) {
   const width = element.clientWidth;
   const height = element.clientHeight;
-  const camera = new THREE.PerspectiveCamera(75, width / height, 1, 4000); // camera 객체 생성
-  camera.position.set(0,0,1200) 
+  const camera = new THREE.PerspectiveCamera(75, width / height, 1, 8000); // camera 객체 생성
+  camera.position.set(0,0,2400) 
   return camera;
 }
