73c19f9d81dba0e089f7e1952e93cc939a08eae4
ldh7228 <ldh7228@gmail.com>
2021-11-28 23:03:43 +0900

    📝 :: [App] Add

--- src/lib/function/camera/index.js
+++ src/lib/function/camera/index.js
@@ -3,12 +3,7 @@ import * as THREE from "three";
 export function SetupCamera(element) {
   const width = element.clientWidth;
   const height = element.clientHeight;
-<<<<<<< HEAD
   const camera = new THREE.PerspectiveCamera(75, width / height, 1, 8000); // camera 객체 생성
   camera.position.set(0,0,2400) 
-=======
-  const camera = new THREE.PerspectiveCamera(75, width / height, 1, 4000); // camera 객체 생성
-  camera.position.set(0,0,500) 
->>>>>>> master
   return camera;
 }
