9ebf6fd75af450dafa436bd8ff7217dd74af5818
eastcopper <ldh7228@gmail.com>
2021-11-28 22:53:22 +0900

    ⚡️ :: [Light] Update lightColor

--- src/lib/function/Light/index.js
+++ src/lib/function/Light/index.js
@@ -1,14 +1,14 @@
 import * as THREE from "three";
 
 export function SetupLight() {
-  const light1Color = 0xb1e1ff; // light blue
-  const light1GroundColor = 0xb97a20; // brownish orange
-  const intensity = 1;
+  const light1Color = 0xffffff;
+  const light1GroundColor = 0xffffff;
+  const intensity = 1
   const light1 = new THREE.HemisphereLight(light1Color, light1GroundColor, intensity);
 
   const light2Color = 0xffffff;
   const light2 = new THREE.DirectionalLight(light2Color, intensity);
-  light2.position.set(5, 10, 2);
+  light2.position.set(0, 0, 0);
 
   const light = {
     light1: light1,
