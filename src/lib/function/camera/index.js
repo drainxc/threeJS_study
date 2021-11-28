ec16cba99ff6444fad7b01cdf7df7281c1be9467
eastcopper <ldh7228@gmail.com>
2021-11-28 22:10:28 +0900

    ⚡️ :: [Camera] Update position

--- src/lib/function/camera/index.js
+++ src/lib/function/camera/index.js
@@ -4,6 +4,6 @@ export function SetupCamera(element) {
   const width = element.clientWidth;
   const height = element.clientHeight;
   const camera = new THREE.PerspectiveCamera(75, width / height, 1, 4000); // camera 객체 생성
-  camera.position.set(0,0,500) 
+  camera.position.set(0,0,1200) 
   return camera;
 }
